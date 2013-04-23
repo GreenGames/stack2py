4424e180e42a1502d3fe21c1b842462c8807f30d
pradyun <pradyunsg@gmail.com>
2013-04-23 14:36:10 +0530

    Made the code cleaner, (D)on't (R)epeat (Y)ourself
    The code now has support for Badges and Tags.

--- source/stack2py.py
+++ source/stack2py.py
@@ -1,23 +1,26 @@
-import urllib2, zlib, ast, pprint, webbrowser
+import urllib2
+import zlib
+import json
+import pprint, webbrowser
 from urllib import urlencode
 
-__version__ = "0.1r02"
+__version__ = "a0.1r3"
 api_url = 'http://api.stackexchange.com/2.1/'
 auth_url = 'https://stackexchange.com/oauth/dialog'
 
 def make_proxy(proxy=None):
     '''Allows for connection through proxy'''
-    proxy_support = urllib2.ProxyHandler(proxy)
-    opener = urllib2.build_opener(proxy_support)
+    proxy = urllib2.ProxyHandler(proxy)
+    opener = urllib2.build_opener(proxy)
     urllib2.install_opener(opener)
     return True
+
 def make_auth_proxy(proxy=None):
     '''Allows for connection through authenticated proxy'''
     proxy = urllib2.ProxyHandler(proxy)
     auth = urllib2.HTTPBasicAuthHandler()
-    opener = urllib2.build_opener(proxy, auth, urllib2.HTTPHandler)
+    opener = urllib2.build_opener(proxy, auth)
     urllib2.install_opener(opener)
-    return True
 
 def make_url(base,kargs={},**kwargs):
     '''creates a url from base and keywords'''
@@ -26,10 +29,7 @@ def make_url(base,kargs={},**kwargs):
 
 def get_dict(url):
     '''Returns the dictionary of the response from url'''
-    return ast.literal_eval(
-        zlib.decompress(
-            urllib2.urlopen(url).read(), 15 + 32)\
-        .replace('false','False').replace('true','True'))
+    return json.loads(zlib.decompress(urllib2.urlopen(url).read(), 15 + 32))
 
 def get_sites():
     '''Returns the name:api site name of all sites on SE'''
@@ -42,31 +42,52 @@ def get_sites():
     return sites
 
 def authenticate():
-    '''Not fully implemented Don't use'''
+    '''Not fully implemented Don't use!!'''
     scope = "write_access,private_info,read_inbox"
     url = make_url(auth_url,client_id=1325,
                    scope=scope,response_type='code',
                    redirect_uri='https://stackexchange.com/oauth/login_success')
     webbrowser.open(url)
 
-def get_token():
-    '''Not fully implemented Don't use'''
-    scope = "write_access,private_info,read_inbox"
-    url = make_url(auth_url,client_id=1325,
-               scope=scope,response_type='code',
-               redirect_uri='https://stackexchange.com/oauth/login_success')
-    print url
-    page = urllib2.urlopen(url)
+class Base:
+    """ The base class for all the other classes """
+    def __init__(self, uid, site):
+        self.uid = uid
+        self.site = site
+    def __repr__(self):
+        return "{0} ID: {1}".format(self.__class__.__name__, self.uid)
 
-class Site:
-    '''A site on SE network'''
+    def set_args(self, dic):
+        for key in dic:
+            setattr(self,key,dic[key])
+
+    def get_info(self,*args,**kwargs):
+        '''Get Information about the Post'''
+        if not args:
+            url = make_url(self.link, kargs=kwargs,site=self.site)
+            print url
+            dic = get_dict(url)
+            self.set_args(dic['items'][0])
+
+        elif isinstance(args[0],dict):
+            try:
+                dic = args[0]
+                self.set_args(dic)
+            except KeyError:
+                print 'Invaluid dictionary given'
+                raise
+        else:
+            raise AttributeError('Invalid arguments')
+
+class Site(Base):
+    '''A site on SE '''
     def __init__(self,site):
         self.site = site
         self.questions = []
-    def __str__(self):
+        self.link = api_url+'info'
+    def __repr__(self):
         return 'Site: '+ self.site
-    __repr__ = __str__
-        
+
     def get_questions(self,**kwargs):
         '''Loads questions into the class'''
         url = make_url(api_url+'questions',kargs=kwargs,
@@ -77,40 +98,14 @@ class Site:
         for i in range(len(self.questions)):
             self.questions[i].get_info(dic['items'][i])
 
-    def get_info(self,*args):
-        '''Get information about site'''
-        if not args:
-            url = make_url(api_url+'info',site=self.site)
-            dic = get_dict(url)
-            self.set_args(dic['items'][0])
-
-        elif isinstance(args[0],dict):
-            try:
-                dic = args[0]
-                self.set_args(dic)
-            except KeyError:
-                print 'Invalid dictionary given'
-                raise
-        else:
-            raise AttributeError('Invalid arguments')
+class Post(Base):
+    """ A post (Question Or Answer) """
+    def __init__(self, uid, site):
+        Base.__init__(self, uid, site)
+        self.comments = []
 
     def set_args(self, dic):
-        for key in dic:
-            setattr(self,key,dic[key])
-
-class Question:
-    '''A Question on SE site'''
-    def __init__(self,uid,site):
-        self.question_id = uid
-        self.site = site
-        self.answers = []
-        self.comment = []
-
-    def __str__(self):
-            return 'Question ID : '+str(self.question_id)
-    __repr__ = __str__
-
-    def set_args(self, dic):
+        """ Set attributes from dict """
         for key in dic:
             setattr(self,key,dic[key])
         if hasattr(self,'owner') and \
@@ -122,27 +117,23 @@ class Question:
             user = User(None,self.site)
             user.get_info(self.owner)
             self.owner = user
-            
-    def get_info(self,*args,**kwargs):
-        '''Get Information about the Question'''
-        if not args:
-            url = make_url(api_url+'questions/'+str(self.question_id),kargs=kwargs,site=self.site,filter='withbody')
-            dic = get_dict(url)
-            self.set_args(dic['items'][0])
 
-        elif isinstance(args[0],dict):
-            try:
-                dic = args[0]
-                self.set_args(dic)
-            except KeyError:
-                print 'Invalid dictionary given'
-                raise
-        else:
-            raise AttributeError('Invalid arguments')
+class User(Base):
+    '''User on SE site'''
+    def __init__(self, uid, site):
+        Base.__init__(self, uid, site)
+        self.link = api_url+'users/'+str(self.uid)
+
+class Question(Post):
+    '''A Question on SE site'''
+    def __init__(self, uid, site):
+        Post.__init__(self, uid, site)
+        self.answers = []
+        self.link = "{0}/questions/".format(self.uid)
 
     def get_answers(self):
         '''Load answers into the class'''
-        url = make_url(api_url+'questions/'+str(self.question_id)+'/answers',
+        url = make_url(self.link+'/answers',
                        site=self.site,filter='withbody')
         dic = get_dict(url)
         for answer in dic['items']:
@@ -152,129 +143,45 @@ class Question:
 
     def get_comments(self):
         '''Load comments into the class'''
-        url = make_url(api_url+'questions/'+str(self.question_id)+'/comments',
+        url = make_url(self.link+'/comments',
                        site=self.site,filter='withbody')
         dic = get_dict(url)
         for comment in dic['items']:
             c = Comment(comment['comment_id'],self.site)
             c.get_info(comment)
             self.comments.append(c)
-        
-class Answer:
-    '''Answer on SE site'''
-    def __init__(self,uid,site):
-        self.answer_id = uid
-        self.site = site
-    def __str__(self):
-            return 'Answer ID : '+str(self.answer_id)
-    __repr__ = __str__
-    
-    def set_args(self, dic):
-        for key in dic:
-            setattr(self,key,dic[key])
-        if hasattr(self,'owner') and \
-           self.owner['user_type'] in ('registered','moderator'):
-            user = User(self.owner['user_id'],self.site)
-            user.get_info(self.owner)
-            self.owner = user
-        else:
-            user = User(None,self.site)
-            user.get_info(self.owner)
-            self.owner = user
 
-    def get_info(self,*args,**kwargs):
-        '''Get Information about the answer'''
-        if not args:
-            url = make_url(api_url+'answers/'+str(self.answer_id),kargs=kwargs,
-                           site=self.site,filter='withbody')
-            dic = get_dict(url)
-            self.set_args(dic['items'][0])
-            
-        elif isinstance(args[0],dict):
-            try:
-                dic = args[0]
-                self.set_args(dic)
-            except KeyError:
-                print 'Invalid dictionary given'
-                raise
-        else:
-            raise AttributeError('Invalid arguments')
+class Answer(Post):
+    '''Answer on SE site'''
+    def __init__(self, uid, site):
+        Post.__init__(self, uid, site)
+        self.link = api_url+'answers/'+str(self.uid)
 
     def get_comments(self):
         '''Load comments into the class'''
-        url = make_url(api_url+'questions/'+str(self.question_id)+'/comments',
+        url = make_url(self.link+'/comments',
                        site=self.site,filter='withbody')
         dic = get_dict(url)
         for comment in dic['items']:
             c = Comment(comment['comment_id'],self.site)
             c.get_info(comment)
             self.comments.append(c)
-class User:
-    '''User on SE site'''
-    def __init__(self,uid,site):
-        self.user_id = uid
-        self.site = site    
-    def __str__(self):
-            return 'User No: '+str(self.user_id)
-    __repr__ = __str__
 
-    def set_args(self, dic):
-        for key in dic:
-            setattr(self,key,dic[key])
-        
-    def get_info(self,*args,**kwargs):
-        '''Get Information about the User '''
-        if not args:
-            url = make_url(api_url+'users/'+str(self.user_id),kargs=kwargs,
-                           site=self.site,filter='withbody')
-            dic = get_dict(url)
-            self.set_args(dic['items'][0])
-        elif isinstance(args[0],dict):
-            try:
-                dic = args[0]
-                self.set_args(dic)
-            except KeyError:
-                print 'Invalid dictionary given'
-                raise
-        else:
-            raise AttributeError('Invalid arguments')
-
-class Comment:
+class Comment(Base):
     '''Comment on SE site'''
-    def __init__(self,uid,site):
-        self.site = site
-        self.comment_id = uid
-    def __str__(self):
-        return 'Comment ID: '+str(self.comment_id)
-    __repr__ = __str__
+    def __init__(self, uid, site):
+        Base.__init__(self, uid, site)
+        self.link = api_url+'comments/'+str(self.uid)
+    set_args = Post.set_args
 
-    def set_args(self, dic):
-        for key in dic:
-            setattr(self,key,dic[key])
-        if hasattr(self,'owner') and \
-           self.owner['user_type'] in ('registered','moderator'):
-            user = User(self.owner['user_id'],self.site)
-            user.get_info(self.owner)
-            self.owner = user
-        else:
-            user = User(None,self.site)
-            user.get_info(self.owner)
-            self.owner = user
+class Badge(Base):
+    def __init__(self, uid, site):
+        Base.__init__(self, uid, site)
+        self.link = api_url+'badges/'+str(self.uid)
+    set_args = Post.set_args
 
-    def get_info(self,*args,**kwargs):
-        '''Get Information about the Comment'''
-        if not args:
-            url = make_url(api_url+'comments/'+str(self.comment_id),kargs=kwargs,
-                           site=self.site,filter='withbody')
-            dic = get_dict(url)
-            self.set_args(dic['items'][0])
-        elif isinstance(args[0],dict):
-            try:
-                dic = args[0]
-                self.set_args(dic)
-            except KeyError:
-                print 'Invalid dictionary given'
-                raise
-        else:
-            raise AttributeError('Invalid arguments')
+class Tag(Base):
+    def __init__(self, uid, site):
+        Base.__init__(self, uid, site)
+        self.link = api_url+'tags/'+str(self.uid)
 
